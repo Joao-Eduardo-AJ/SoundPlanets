5f837b43215e5f90bd388ed6ad0ae15bfabf1e5c
@jardim <joao.jardim@syonet.com>
2023-08-07 22:12:37 -0300

    feat: add eslint and prettier for lint and indentation

--- App.tsx
+++ App.tsx
@@ -1,36 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import React from "react";
+import { Text, View } from "react-native";
 
 function App(): JSX.Element {
-
   return (
-        <View>
-          <Text>Hello World!</Text>
-        </View>
+    <View>
+      <Text>Hello World!</Text>
+    </View>
   );
 }
 
